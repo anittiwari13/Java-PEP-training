70242bd3049c3f276ebd27b9211e9eb62b89b723
ACER <tiwarianit111@gmail.com>
2022-01-25 22:26:46 +0530

    mutable strings

---
+++ src/session6/assignmentStringBuffer.java
@@ -0,0 +1,36 @@
+package session6;
+
+public class assignmentStringBuffer {
+    public static void main(String[] args) {
+        //String Buffer
+        StringBuffer sb = new StringBuffer();
+
+        System.out.println(sb.capacity());
+        sb.append("This is a String");
+        System.out.println(sb);
+        sb.append(" Let's run this Code");
+        System.out.println(sb);
+
+        sb.reverse();        //mutable
+        System.out.println("Revered stringBuffer----- "+sb);
+
+        //delete method
+        sb.reverse();
+        sb.delete(27,31);
+        System.out.println(sb);
+
+        StringBuffer str = new StringBuffer("Program");
+        str.replace(1,4,"Master");
+        System.out.println(str);
+
+        StringBuffer str1 = new StringBuffer("Program");
+        System.out.println(str1);
+        str1.insert(7," this problem");
+        System.out.println("Inserted 'this problem at idx 7 : "+str1);
+
+        System.out.println("Capacity before ensureCapacity : "+sb.capacity());
+
+        sb.ensureCapacity(38);
+        System.out.println("Capacity after ensureCapacity : "+sb.capacity());
+    }
+}
